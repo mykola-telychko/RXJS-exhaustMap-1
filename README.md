f482957d0f8c1612ccf6ca8be108d10dafb4753b
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-31 01:19:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-exhaustMap-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-1d3fhm)
---
+++ index.ts
@@ -0,0 +1,34 @@
+import { interval } from 'rxjs';
+import { exhaustMap, tap, take } from 'rxjs/operators';
+
+const firstInterval = interval(1000).pipe(take(10));
+const secondInterval = interval(1000).pipe(take(2));
+
+const exhaustSub = firstInterval
+  .pipe(
+    exhaustMap((f) => {
+      console.log(`Emission Corrected of first interval: ${f}`);
+      return secondInterval;
+    })
+  )
+  .subscribe((s) => console.log(s));
+/*
+                When we subscribed to the first interval, it starts to emit a values (starting 0).
+                This value is mapped to the second interval which then begins to emit (starting 0).  
+                While the second interval is active, values from the first interval are ignored.
+                We can see this when firstInterval emits number 3,6, and so on...
+
+                  Output:
+                  Emission of first interval: 0
+                  0
+                  1
+                  Emission of first interval: 3
+                  0
+                  1
+                  Emission of first interval: 6
+                  0
+                  1
+                  Emission of first interval: 9
+                  0
+                  1
+              */
